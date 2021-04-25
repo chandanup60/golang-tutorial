7524a5095007e9f6136ed923d09ec2728dccea0c
chandanup60 <83165263+chandanup60@users.noreply.github.com>
2021-04-25 17:00:58 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+   /* This is my first sample program. */
+  /* {
+	int n;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number");
+	n=sc.nextInt();
+	if(n%2==0)
+	{
+	System.out.println("Even Number");
+	}
+	else
+	{
+	System.out.println("Odd Number");
+	}
+	} */
+	/*
+     var number1 int;
+	fmt.Scanf("%d", &number1);
+		var n,m int =1,1;
+
+		for n<=number1 {
+		m=m*n;
+		n=n+1;
+		} 
+		fmt.Println(m); */
+	
+		
+var  a,b,c int=0,1,0;
+fmt.Print(a +b);
+for c<13 {
+c=a+b;
+fmt.Print(c);
+a=b;
+b=c;
+}
+
+
+}
+
